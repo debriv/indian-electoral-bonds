9c3f46d91e86e54715bf790ca40c90681e5819be
debriv <debarghya@yahoo.com>
2024-03-26 02:49:33 +0530

    Pushing the Parsed PDF to csv code and datafiles

--- pdf_parser/main.py
+++ pdf_parser/main.py
@@ -1,12 +1,31 @@
 import tabula
 import warnings
+import pandas
+
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-import pandas
-pdf_path = "ec_released_bond_data_files/electoral_bonds_disclosure_21_1.pdf"
+base_source_path = "ec_released_bond_data_files/"
+base_destination_path = "pdf_parser/parsed_pdfs/"
+file_name_list = [
+    "electoral_bonds_disclosure_21_1",
+    "electoral_bonds_disclosure_21_2",
+    "electoral_bonds_disclosure_sbi_1",
+    "electoral_bonds_disclosure_sbi_2"
+                  ]
 
-# dfs = tabula.read_pdf(pdf_path, pages=3, stream=True)
-# # read_pdf returns list of DataFrames
-tabula.convert_into(pdf_path, "pdf_parser/parsed_pdfs/electoral_bonds_disclosure_21_1.csv", pages = 'all'
-                    ,output_format="csv", stream=True)
-# print(dfs)
+for file_name in file_name_list:
+    print(
+        f"{base_source_path}{file_name}.pdf",
+        " started to be parsed into ", 
+        f"{base_destination_path}{file_name}.csv"
+        )
+    tabula.convert_into(f"{base_source_path}{file_name}.pdf", 
+                        f"{base_destination_path}{file_name}.csv", 
+                        pages = 'all',
+                        output_format="csv", 
+                        stream=True)
+    print(
+        f"{base_source_path}{file_name}.pdf",
+        " parsing is complete and the csv is available at ", 
+        f"{base_destination_path}{file_name}.csv"
+        )
